3321528b703ba8ebc6fc31ac01265e12d60402fd
Léo Belzile <belzilel@gmail.com>
2022-11-11 00:02:46 -0500

    Slides 10

--- example/mixed_models.R
+++ example/mixed_models.R
@@ -1,4 +1,19 @@
-```{r}
+library(lmerTest)
+# TV data from Bang & Olufsen
+data(TVbo, package = "lmerTest")
+# Crossed factors (Picture and TVset)
+# 15 response variables (characteristics of product)
+# these are nested within assessors (8)
+fm <- lmer(Coloursaturation ~ TVset * Picture + 
+       (1 | Assessor) + 
+       (1 | Assessor:TVset) + 
+       (1 | Assessor:Picture), data = TVbo)
+ranova(fm)
+anova(fm, ddf = "Kenward-Roger")
+
+fm <- aov(Coloursaturation ~ TVset * Picture + Error(Assessor/(Picture*TVset)), data = TVbo)
+
+
 library(afex) # analysis of factorial experiments
 library(emmeans) # estimated marginal means
 # Set up sum-to-zero constraint for factors
@@ -20,4 +35,3 @@ model <- lme4::lmer(guilt ~ anchor*verdictsyst +
                     data = C22)
 car::Anova(model, test.statistic = "F")
 emmeans(model, spec = "anchor") |> contrast(method = "pairwise")
-```
