a2e5dc9dd55830a4dca58d6dd1ed9a372a203b82
Léo Belzile <belzilel@gmail.com>
2024-02-12 07:37:07 -0500

    Add problem sets

---
+++ content/MATH80667A-06-content.R
@@ -0,0 +1,70 @@
+library(ggplot2) # grammar of graphics
+library(dplyr) # data manipulation
+library(car) # regresion tools
+library(emmeans) # estimated marginal means
+# Load data
+data(popcorn, package = 'hecedsm')
+# Fit model with three-way interaction
+model <- aov(percentage ~ brand*power*time,
+             data = popcorn)
+# ANOVA table - 'anova' function is ONLY for balanced designs
+anova_table <- anova(model)
+car::Anova(model, type = 3)
+# Because of balancing, can analyze all effects
+# No three-way interaction
+# Only two-way interaction is brand*time combo (and weak evidence)
+
+# Quantile-quantile plot
+car::qqPlot(model)
+# Interaction plot
+popcorn |>
+  group_by(brand, time, power) |>
+  summarize(meanp = mean(percentage)) |>
+  ggplot(mapping = aes(x = power,
+                       y = meanp,
+                       col = time,
+                       group = time)) +
+  geom_line() +
+  facet_wrap(~brand)
+
+## Questions
+# Q1: Which combo (brand, power, time) gives highest popping rate? (pairwise comparisons of all combos)
+cellmeans <- emmeans(model, specs = c("brand","power","time"))
+# There are 153 pairs!
+choose(n = 18, k =  2)
+pairs(cellmeans)
+# Q2: Best brand overall (marginal means marginalizing over power and time, assuming no interaction)
+margmean_brand <- emmeans(model, specs = "brand")
+margmean_brand |> contrast("pairwise")
+# National 1 has the highest average, but not significantly difference from others
+# The main culprit is the high standard error, due to r=2 replications.
+# We only have 12 observations per group, but standard dev has 18 degrees of freedom.
+
+# Q3: Effect of time and power on percentage of popped kernels
+# pairwise comparison of time x power
+emmeans(model, specs = c("time", "power")) |> # specs is what we keep.
+  # 4m30s 600W is highest mean
+  pairs()
+
+
+# Q4: main effect of power
+emmeans(model, specs = "power") |>
+  pairs()  # shortcut for contrast("pairwise")
+# Note how this differs from the ANOVA table (because of averaging)
+# The reason for this discrepancy is that we compare different models!
+
+
+# Q5: main effect of time
+# Rather than global, we show how to look at this separately for each brand
+emmeans(model, specs = "time", by = "brand") |>
+  pairs() |>
+  test(joint = TRUE)
+# Q6: Interaction contrasts for two-way ANOVA
+# Compare difference between percentage of popped kernels
+#  for 4.5 versus 5 minutes, for brands 1 and 2
+emm_popcorn_AC <- emmeans(model,
+                          specs = c("brand","time"))
+contrast_list <- list(
+    brand12with4.5vs5min = c(0, 0, 0, 1, -1, 0, -1, 1,0))
+contrast(emm_popcorn_AC,  # marginal mean (no time)
+         method = contrast_list) # list of contrasts
